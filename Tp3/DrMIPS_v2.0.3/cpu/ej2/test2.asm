1697a291452e65a3923add1b3e8ac35c961e6a23
Constanza Frutos <constanza.frutos.ramos@gmail.com>
2019-06-14 20:49:33 -0300

    Funciona sll para uniciclo, hay que arreglar vista

--- Tp3/DrMIPS_v2.0.3/cpu/ej2/test2.asm
+++ Tp3/DrMIPS_v2.0.3/cpu/ej2/test2.asm
@@ -1,5 +1,10 @@
 li $t0, 10
-sll $t1, $t0, 1
-sll $t2, $t0, 2
-sll $t3, $t0, 3
-sll $t4, $t0, 4
+li $t1, 2
+li $t2, 4
+add $t0, $t1, $t2
+li $a1, 1
+
+sll $t1, $t0, 5
+#sll $t2, $t0, 2
+#sll $t3, $t0, 3
+#sll $t4, $t0, 4
